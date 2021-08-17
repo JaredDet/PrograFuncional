d8930aa87fb359f83bbb701ab91a7e6a742590ca
Javier Marquez <jamarquez145@gmail.com>
2021-08-17 06:26:47 -0400

    Uso de stream de Java

---
+++ src/main/java/lanzador/v11_streams/Descripcion.java
@@ -0,0 +1,19 @@
+package lanzador.v11_streams;
+
+public class Descripcion {
+
+    private final Integer valor;
+
+    public Descripcion(Integer valor){
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "V(" + valor +")";
+    }
+}
